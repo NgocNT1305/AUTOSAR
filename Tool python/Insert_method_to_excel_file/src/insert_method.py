370945ed86776522eca304028c4587fb8345a0c2
DungHa <dung20155269@gmail.com>
2024-08-11 19:36:16 +0700

    add file of tool excel

---
+++ Tool python/Insert_method_to_excel_file/src/insert_method.py
@@ -0,0 +1,40 @@
+import os
+import pandas as pd
+from openpyxl import load_workbook
+
+def manipulate_excel_file(directory, filename):
+    """Thao tác với file Excel trong thư mục để kiểm tra sheet và chèn công thức."""
+    
+    file_path = os.path.join(directory, filename)
+
+    # Kiểm tra xem file có tồn tại không
+    if not os.path.isfile(file_path):
+        print(f"File {file_path} không tồn tại.")
+        return
+
+    # Load workbook và kiểm tra sheet
+    try:
+        workbook = load_workbook(file_path, data_only=False)
+        sheet_names = workbook.sheetnames
+        
+        if 'STD' not in sheet_names:
+            print("Không có sheet STD.")
+            return
+
+        # Lấy sheet STD
+        sheet = workbook['STD']
+
+        # Thêm công thức vào cột T từ hàng 2 đến hết
+        for row in range(2, sheet.max_row + 1):
+            formula = f'="STD-"&D{row}&"-"&F{row}&"-"&H{row}'
+            sheet[f'T{row}'] = formula
+
+        # Lưu workbook sau khi thay đổi
+        workbook.save(file_path)
+        print(f"Đã cập nhật file: {file_path}")
+
+    except Exception as e:
+        print(f"Không thể thao tác với file {file_path}: {e}")
+
+def insert_method_main(output_dir):
+    manipulate_excel_file(output_dir)
--- Tool python/Insert_method_to_excel_file/src/main.py
+++ Tool python/Insert_method_to_excel_file/src/main.py
@@ -1,3 +1,10 @@
-import os
-#update main branch not include change of DungHVH branch
-#update main.py from DungHVH
+import app
+ 
+
+if __name__ == "__main__":
+
+    file_name = 'dung'
+
+    output_dir = app.app_main(file_name)
+
+    print(file_name)
